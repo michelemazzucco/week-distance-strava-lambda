3d37cb179fa857ce91dbd6ef3285a27318af27f2
Michele Mazzucco <hello@michelemazzucco.it>
2018-01-19 22:11:50 +0100

    promisify https, refactoring handler

--- utils.js
+++ utils.js
@@ -1,4 +1,19 @@
-const parseDate = date => Math.round(date / 1000)
+const https = require('https')
+
+const getData = url => {
+  return new Promise((resolve, reject) => {
+    const req = https.get(url, res => {
+      const code = res.statusCode
+      let body = []
+      
+      if (code < 200 || code > 299) reject(new Error(code))
+      res.on('data', chunk => body.push(chunk))
+      res.on('end', () => resolve(body))
+    })
+
+    req.on('error', (err) => reject(err))
+  })
+}
 
 const getMonday = () => {
   const today = new Date()
@@ -8,13 +23,25 @@ const getMonday = () => {
   return new Date(today.setDate(diff)).getTime()
 }
 
+const setResponse = (statusCode, body) => ({
+  statusCode: statusCode,
+  headers: {
+    "Access-Control-Allow-Origin" : "*",
+  },
+  body: JSON.stringify(body)
+})
+
 const sumDistance = arr => arr.reduce((a, v) => a + v.distance, 0)
 
-const parseDistance = distance => Math.floor((distance / 1000) * 100) / 100
+const parseDistance = d => Math.floor((d / 1000) * 100) / 100
+
+const parseDate = d => Math.round(d / 1000)
 
 module.exports = {
-  parseDate: parseDate,
+  getData: getData,
   getMonday: getMonday,
+  setResponse: setResponse,
   sumDistance: sumDistance,
-  parseDistance: parseDistance
+  parseDistance: parseDistance,
+  parseDate: parseDate,
 }
